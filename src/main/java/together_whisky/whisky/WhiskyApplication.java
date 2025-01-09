6359f3ab47967674d854f01ab77cdca9e539ee4b
br12345678 <mystyle730@gmail.com>
2025-01-09 22:35:06 +0900

    init

---
+++ src/main/java/together_whisky/whisky/WhiskyApplication.java
@@ -0,0 +1,13 @@
+package together_whisky.whisky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiskyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/together_whisky/whisky/WhiskyApplicationTests.java
@@ -0,0 +1,13 @@
+package together_whisky.whisky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiskyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
